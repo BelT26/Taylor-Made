0b5da4e69c26ef7a0b65cb8bc4f31abe48fab71c
Helen Taylor <helen.taylor@hotmail.it>
2023-02-18 08:53:54 +0000

    update css import statement in Header.js

--- components/page_layout/header.js
+++ components/page_layout/header.js
@@ -3,7 +3,7 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Link from "next/link";
-import classes from "./Header.module.css";
+import classes from "./header.module.css";
 
 function CustomLink({ href, children }) {
   return (
